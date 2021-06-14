61784bf6642103160802a998627e13f59b509a2d
1NT18IS064 <82821462+1NT18IS064@users.noreply.github.com>
2021-06-14 07:43:28 +0530

    Routing

---
+++ Routing/routing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+struct node
+{
+    unsigned dist[20];
+    unsigned from[20];
+} rt[10];
+int main()
+{
+    int costmat[20][20];
+    int nodes, i, j, k, count = 0;
+    printf("\nEnter the number of nodes greater than three: ");
+    scanf("%d", &nodes);
+    printf("\nEnter the cost matrix :\n");
+    for (i = 0; i < nodes; i++)
+    {
+        for (j = 0; j < nodes; j++)
+        {
+            scanf("%d", &costmat[i][j]);
+        }
+    }
+}
